d49d454ff65ed66c8a17c7ad9be611c93d4dea6f
Alexey Melezhik <melezhik@gmail.com>
2017-08-10 17:22:04 +0300

    handling (.) case

--- zef/story.bash
+++ zef/story.bash
@@ -5,10 +5,15 @@ set -e
 export PATH=/opt/rakudo/bin:~/.perl6/bin:$PATH
 
 options=$(config options)
+list=$(config list)
 
-for m in $(config list); do
+for m in $list; do
   echo zef install $m $options
-  zef install $m $options
+  if test $m == '(.)'; then
+    zef install . $options
+  else
+    zef install $m $options
+  fi
 done
 
 
