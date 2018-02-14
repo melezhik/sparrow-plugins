e0450c1413ba6e20efb5e62ef58e3b9c4dbda613
Alexey Melezhik <melezhik@gmail.com>
2018-02-14 11:25:42 +0300

    absolute path to ssl.conf

--- uwp-selfsigned-cert/story.bash
+++ uwp-selfsigned-cert/story.bash
@@ -9,7 +9,7 @@ set -x
 
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days $days \
 -nodes -subj $subj \
--extensions ext -config ssl.conf
+-extensions ext -config $project_root_dir/ssl.conf
 
 openssl pkcs12 -passout pass: -inkey key.pem -in cert.pem -export -out cert.pfx
 
