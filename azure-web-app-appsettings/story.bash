75e2487a4aa0a3b5730c7c0ab7ab3e5f4db6c64b
Alexey Melezhik <melezhik@gmail.com>
2018-05-04 19:37:46 +0000

    nicer format

--- azure-web-app-appsettings/story.bash
+++ azure-web-app-appsettings/story.bash
@@ -4,8 +4,22 @@ app_service=$(config app_service)
 
 group=$(config group)
 
+echo "================================================="
 echo "AppService: ${app_service}"
 echo "ResourceGroup: ${group}"
-echo "==============================================="
+echo "================================================="
 
-az webapp config appsettings list  -n "${app_service}"  -g  "${group}"
+
+az webapp config appsettings list  -n "${app_service}"  -g  "${group}" -o json | perl -MJSON -e '
+  my $str = join "", <STDIN>;
+  my $d = decode_json($str);
+  for $i ( sort {$a->{name} cmp $b->{name} } @{$d} ){
+    write
+  }
+
+format STDOUT = 
+^>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  @<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+$i->{name} , $i->{value}
+.
+'
+
