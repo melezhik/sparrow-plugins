1790cc9669abf6be4f05f6f85f794bb5e452f4fd
Alexey Melezhik <melezhik@gmail.com>
2018-05-04 15:04:11 +0000

    small changes

--- azure-web-app-appsettings/story.bash
+++ azure-web-app-appsettings/story.bash
@@ -1,8 +1,11 @@
 set -e
-set -x
 
-app_service=$(cofnig app_service)
+app_service=$(config app_service)
 
-group=$(cofnig group)
+group=$(config group)
+
+echo "AppService: ${app_service}"
+echo "ResourceGroup: ${group}"
+echo "==============================================="
 
 az webapp config appsettings list  -n "${app_service}"  -g  "${group}"
