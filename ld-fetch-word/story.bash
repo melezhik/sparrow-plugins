b144c463f394dc752a9dcda774ba609842e3a13c
Alexey Melezhik <melezhik@gmail.com>
2018-07-13 16:12:56 +0000

    functions

--- ld-fetch-word/story.bash
+++ ld-fetch-word/story.bash
@@ -9,6 +9,11 @@ sleep $sleep
 
 echo ============================================================
 
+echo >> $out
+echo >> $out
+
+echo "{{{ $word }}}" >> $out 
+
 curl https://www.ldoceonline.com/dictionary/$word -s -L | perl -n -e '
 print $_, "\n" for /title="Play Example"> <\/span>(.*?)<\/span>/mg' | \
 perl -n -e  's/<span.*>//; print' | tee -a  $out
