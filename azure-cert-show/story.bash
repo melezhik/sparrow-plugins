7bf9cc11b54795b9451f5f6df8507dbfd6d85939
Alexey Melezhik <melezhik@gmail.com>
2018-08-30 15:37:22 +0000

    new version

--- azure-cert-show/story.bash
+++ azure-cert-show/story.bash
@@ -9,11 +9,11 @@ az keyvault certificate show  --vault-name $vault_name -n $cert_name -o json | \
 perl -MJSON -Mstrict -e '
   my $json = join "", <STDIN>;
   my $data = decode_json($json);
-  print "Vault Name:\t", $ENV{vault_name}, "\n";
-  print "Cert Name:\t", $ENV{cert_name}, "\n";
+  print "Vault Name:\t\t", $ENV{vault_name}, "\n";
+  print "Cert Name:\t\t", $ENV{cert_name}, "\n";
   print "x509ThumbprintHex:\t", $data->{x509ThumbprintHex}, "\n";
-  print "Expires:\t", $data->{attributes}->{expires}, "\n";
-  print "dnsNames:\t", join " ", @{$data->{policy}->{'x509CertificateProperties'}->{subjectAlternativeNames}->{dnsNames}}, "\n";
+  print "Expires:\t\t", $data->{attributes}->{expires}, "\n";
+  print "dnsNames:\t\t", join " ", @{$data->{policy}->{'x509CertificateProperties'}->{subjectAlternativeNames}->{dnsNames}}, "\n";
   
 '
 echo "==================================================="
