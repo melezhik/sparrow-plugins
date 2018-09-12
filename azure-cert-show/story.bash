cc51c7257f1ff3d342f718a830750b27e01abc6a
Alexey Melezhik <melezhik@gmail.com>
2018-09-12 14:31:40 +0000

    check thumbprint option

--- azure-cert-show/story.bash
+++ azure-cert-show/story.bash
@@ -4,6 +4,8 @@ set -e;
 
 export vault_name=$(config vault_name)
 export cert_name=$(config cert_name)
+export check_thumbprint=$(config check_thumbprint)
+
 echo "==================================================="
 az keyvault certificate show  --vault-name $vault_name -n $cert_name -o json | \
 perl -MJSON -Mstrict -e '
@@ -14,6 +16,10 @@ perl -MJSON -Mstrict -e '
   print "x509ThumbprintHex:\t", $data->{x509ThumbprintHex}, "\n";
   print "Expires:\t\t", $data->{attributes}->{expires}, "\n";
   print "dnsNames:\t\t", join " ", @{$data->{policy}->{'x509CertificateProperties'}->{subjectAlternativeNames}->{dnsNames}}, "\n";
+  print "===================================================\n\n";
+  if ($ENV{check_thumbprint}){
+    print "x509ThumbprintHex does not equal to the passed one ($ENV{check_thumbprint})\nconsider updating your certifcates\n" and exit 1 
+      if $ENV{check_thumbprint} ne $data->{x509ThumbprintHex};
+  }
   
 '
-echo "==================================================="
