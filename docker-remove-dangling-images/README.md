97591081397d0e24274b44086a80f702bb2056ca
Alexey Melezhik <melezhik@gmail.com>
2018-09-13 14:17:27 +0000

    final version

---
+++ docker-remove-dangling-images/README.md
@@ -0,0 +1,16 @@
+# SYNOPSIS
+
+Remove dangling docker images and stopped containers.
+
+# INSTALL
+
+    $ sparrow plg install docker-remove-dangling-images
+
+# USAGE
+
+    $ sparrow plg run docker-remove-dangling-images
+
+# Author
+
+Alexey Melezhik
+
--- docker-remove-dangling-images/hook.pl
+++ docker-remove-dangling-images/hook.pl
@@ -2,20 +2,20 @@
 
 use strict;
 
-open(my $docker_c_list, '-|', 'docker', 'ps', -f 'status=exited', '-q', '--no-trunc' ) or die $!;
+open(my $docker_c_list, '-|', 'docker', 'ps', '-f', 'status=exited', '-q', '--no-trunc' ) or die $!;
 
 while (<$docker_c_list>) {
    chomp;
-   run_story("rm_container", { container => $_ });
+   run_story("rm-container", { container => $_ });
 }
 
 close($docker_c_list);
 
-open(my $docker_im_list, '-|', 'docker', 'images', -f 'dangling=true', '-q', '--no-trunc' ) or die $!;
+open(my $docker_im_list, '-|', 'docker', 'images', '-f', 'dangling=true', '-q', '--no-trunc' ) or die $!;
 
 while (<$docker_im_list>) {
    chomp;
-   run_story("rm_image", { image => $_ });
+   run_story("rm-image", { image => $_ });
 }
 
 close($docker_im_list);
