416b5cf220a25e9f0419417dad72fda0b032bcbf
Alexey Melezhik <melezhik@gmail.com>
2018-12-12 12:45:52 -0600

    new plugin

---
+++ vsts-run-build/modules/run/story.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+definition=$(config definition)
+
+branch=$(git symbolic-ref HEAD 2>/dev/null)
+
+echo vsts build queue --definition-name $definition --branch $branch
+
+vsts build queue --definition-name $definition --branch $branch
+
+#vsts build list | head
+
