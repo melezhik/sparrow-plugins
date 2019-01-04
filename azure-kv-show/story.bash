41e6855461f03f80106bc0cbae8ebfb1eea73ac9
Alexey Melezhik <melezhik@gmail.com>
2019-01-04 21:23:01 +0000

    new version of azure kv show

--- azure-kv-show/story.bash
+++ azure-kv-show/story.bash
@@ -4,16 +4,32 @@ url=$(config url)
 
 if test "${url}"; then
   kv=`perl -e '$ARGV[0]=~/https:\/\/(\S+)\.vault\.azure\.net/; print $1' $url`  
-  sec=`perl -e '$ARGV[0]=~/.*\/(\S+)/; print $1' $url`  
+  secret=`perl -e '$ARGV[0]=~/.*\/(\S+)/; print $1' $url`  
 else
   kv=$(config kv)
-  sec=$(config sec)
+  secret=$(config secret)
 fi
 
 
-echo "================================================="
-echo "Key Vault: ${kv}"
-echo "Secret Name: ${sec}"
-echo "================================================="
+az keyvault secret show --name "${secret}" --vault-name "${kv}" -o json > $cache_dir/kv.json
 
-az keyvault secret show --name "${sec}" --vault-name "${kv}"
+perl -Mstrict -MJSON -e '
+
+  my $file = $ARGV[0];
+  my $kv = $ARGV[1];
+  my $secret = $ARGV[2];
+
+  open my $fh, $file or die "cannot open file $file to read: $!";
+  my $s = join "", <$fh>;
+  close $fh;
+  #print $s;
+
+  my $data = decode_json($s);
+
+  print "=========================\n";
+  print "key vault: $kv\n";
+  print "   secret: $secret\n"; 
+  print "    value: $data->{value}\n";
+  print "=========================\n";
+
+' $cache_dir/kv.json $kv $secret
