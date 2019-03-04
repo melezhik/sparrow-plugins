3d060a01ec2013ce2395a73f65890ba4fe1d3755
Alexey Melezhik <melezhik@gmail.com>
2019-03-04 22:03:38 +0000

    limit accounts list to the current one

--- azure-account-set/story.bash
+++ azure-account-set/story.bash
@@ -4,13 +4,13 @@ set -e
 
 subscription=$(config subscription)
 
-
 if test -z "${subscription}"; then
   az account list -o table
 else
   set -x
+  echo "set subscription: ${subscription} ..."
   az account set --subscription $subscription -o table
-  az account list
+  az account list | grep $subscription
 fi
 
 
