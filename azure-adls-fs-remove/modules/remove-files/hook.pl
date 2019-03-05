4a99a6767724220443deed03e3d3a56129f42b65
Alexey Melezhik <melezhik@gmail.com>
2019-03-05 23:51:11 +0000

    show counters

--- azure-adls-fs-remove/modules/remove-files/hook.pl
+++ azure-adls-fs-remove/modules/remove-files/hook.pl
@@ -1,5 +1,9 @@
 my $state = get_state();
 
+my $cnt = scalar @{$state->{files}};
+
+set_stdout "files to remove: $cnt";
+
 for my $f (reverse sort @{$state->{files}}) {
 
   run_story("remove-fs", { path => $f });
--- azure-adls-fs-remove/modules/remove-folders/hook.pl
+++ azure-adls-fs-remove/modules/remove-folders/hook.pl
@@ -1,5 +1,9 @@
 my $state = get_state();
 
+my $cnt = scalar @{$state->{dirs}};
+
+set_stdout "folders to remove: $cnt";
+
 for my $f (reverse sort @{$state->{dirs}}) {
 
   run_story("remove-fs", { path => $f });
