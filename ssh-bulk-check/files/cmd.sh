4bf87dd3a67f53b39759b6194a96360a6cfba6f6
Alexey Melezhik <melezhik@gmail.com>
2019-04-29 18:48:23 +0000

    working version

--- ssh-bulk-check/files/cmd.sh
+++ ssh-bulk-check/files/cmd.sh
@@ -1,1 +1,2 @@
-sudo ls -l /var
+ls -d /var/data && echo "/var/data is a directory"
+ls -l /rrr/
--- ssh-bulk-check/modules/check-host/story.bash
+++ ssh-bulk-check/modules/check-host/story.bash
@@ -1,11 +1,11 @@
 #!bash
 
 if test -z $password; then
-    cat $cmd | ssh $host -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
+    cat $cmd | ssh $host -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
 else
   if test -z $user; then
-    cat $cmd | sshpass -p $password ssh $host -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
+    cat $cmd | sshpass -p $password ssh $host -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
   else
-    cat $cmd | sshpass -p $password ssh -l $user $host -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
+    cat $cmd | sshpass -p $password ssh -l $user $host -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
   fi
 fi
