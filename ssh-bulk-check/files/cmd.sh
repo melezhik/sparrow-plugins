1b705706ee921fdb281cd8995ee3422631b0308d
Alexey Melezhik <melezhik@gmail.com>
2019-04-29 20:04:25 +0000

    more examples

--- ssh-bulk-check/files/cmd.sh
+++ ssh-bulk-check/files/cmd.sh
@@ -1,5 +1,18 @@
+echo '==='
+
 ls -d /var/data && echo "/var/data is a directory"
 
-echo "/tmp/ dir size"
-sudo du -sh /tmp/
+echo '==='
+
+echo "check /tmp/ dir size"
+  sudo du -sh /tmp/
 echo "end check"
+
+
+echo '==='
+
+echo "check if nginx is alive"
+  ps uax| grep nginx| grep -v grep
+echo "end check"
+
+echo '==='
