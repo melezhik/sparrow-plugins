ec7f751e422275ed24f83867f8bf3cb5ad5a9622
Alexey Melezhik <melezhik@gmail.com>
2019-05-03 19:42:19 +0000

    print name

--- ado-read-variable-groups/modules/parse-data/task.pl
+++ ado-read-variable-groups/modules/parse-data/task.pl
@@ -11,7 +11,14 @@ my $data = join "", <$fh>;
 
 close $fh; 
 
-my %vars = %{from_json($data)->{variables}};
+my $data = from_json($data);
+
+#use Data::Dumper;
+
+#print Dumper($data);
+
+my %vars = %{$data->{variables}};
+my $group_name = $data->{name}; 
 
 my @rows = ([qw/name value/]);
 my @list;
@@ -24,6 +31,8 @@ for my $i (sort keys %vars){
   push @list, { name => $i, value => $vars{$i}->{value} };
   push @rows, [ $i, $vars{$i}->{value} ];
 }
+
+print "================[ $group_name] =================\n";
 
 print generate_table(rows => \@rows, header_row => 1, separate_rows => 1);
 
