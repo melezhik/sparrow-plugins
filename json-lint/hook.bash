59abe2b5ff308360180bc6e197a55eb4fc2ada54
Alexey Melezhik <melezhik@localhost>
2019-06-04 20:05:06 +0000

    remove sparrow_version from sparrow.json

--- json-lint/hook.bash
+++ json-lint/hook.bash
@@ -1,9 +1,27 @@
+#!bash
+
 path=$(config path)
 
-for f in $(ls $path/*.json); do
-  run_task lint path $f 
-done
+if test -z $path; then
 
-for f in $(ls $path/*.js); do
-  run_task lint path $f 
-done
+  for f in $(ls $PWD/*.json); do
+    run_task lint path $f 
+  done
+  
+  for f in $(ls $PWD/*.js); do
+    run_task lint path $f 
+  done
+  
+else
+
+  for f in $(ls $path/*.json); do
+    run_task lint path $f 
+  done
+  
+  for f in $(ls $path/*.js); do
+    run_task lint path $f 
+  done
+  
+fi
+
+
