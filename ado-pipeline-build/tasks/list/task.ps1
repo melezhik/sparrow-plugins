c935beed8e9dc6d14ecc63ffdea6e17448a7762c
Aleksei Melezhik <melezhik@gmail.com>
2019-09-25 13:21:13 -0500

    overcome powershell quirks

--- ado-pipeline-build/tasks/list/task.ps1
+++ ado-pipeline-build/tasks/list/task.ps1
@@ -1,3 +1,3 @@
 $branch = git symbolic-ref HEAD
 $name = config name
-az pipelines build list --branch $branch | Select-String -Pattern "\s${name}\s|^ID "
+az pipelines build list --branch $branch | Select-String -Pattern "\s${name}\s|^ID|^\-" | % { $_.Line }
