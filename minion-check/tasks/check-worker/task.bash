24ce39dc2e0f22a0e072351816f1f400caff665a
Alexey Melezhik <melezhik@gmail.com>
2019-12-30 20:14:11 -0600

    the plugin is deprecated

--- minion-check/tasks/check-worker/task.bash
+++
@@ -1,7 +0,0 @@
-worker_cmd=$(config worker_cmd)
-
-echo look up "{{{$worker_cmd}}}" at proccess list ...
-
-ps uax | grep "$worker_cmd" | grep -v grep || exit 2
-
-
--- minion-check/tasks/last-task/task.bash
+++
@@ -1,7 +0,0 @@
-cmd=$(config command)
-
-if eval "$cmd job -S failed -l 1" | grep failed; then
-  echo Q=1
-else
-  echo Q=0
-fi
