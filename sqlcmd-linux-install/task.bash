a5c87edee025fa2d967cfb6acf6449a91bf87662
Alexey Melezhik <melezhik@gmail.com>
2020-04-09 20:16:05 +0000

    new plugin

--- sqlcmd-linux-install/task.bash
+++ sqlcmd-linux-install/task.bash
@@ -1,6 +1,7 @@
 set -x
 set -e
 curl -sLf https://packages.microsoft.com/config/rhel/7/prod.repo > /etc/yum.repos.d/msprod.repo
-yum install mssql-tools unixODBC-devel
+ACCEPT_EULA=Y
+yum install mssql-tools unixODBC-devel -y
 PATH=$PATH:/opt/mssql-tools/bin sqlcmd -?
 
