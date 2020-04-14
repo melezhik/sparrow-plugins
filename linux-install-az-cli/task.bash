df64755b1cca62320e418e89788ef8fc422b75ee
Alexey Melezhik <melezhik@gmail.com>
2020-04-14 16:00:26 +0000

    new plugin

---
+++ linux-install-az-cli/task.bash
@@ -0,0 +1,8 @@
+set -e
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[azure-cli]
+name=Azure CLI
+baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo
