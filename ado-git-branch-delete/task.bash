d36abfba292ad33cc6a5df4a4edc1eae1118b2d4
Alexey Melezhik <melezhik@gmail.com>
2020-06-17 21:06:33 -0400

    new version

---
+++ ado-git-branch-delete/task.bash
@@ -0,0 +1,25 @@
+set -e
+
+. ~/.sparrow-azure-devops.sh
+
+cp -v $task_dir/template.json $cache_dir/request.json
+
+project=$(config project)
+repo=$(config repo)
+branch=$(config branch)
+
+perl -i -p -e "s/%branch%/$branch/g;" $cache_dir/request.json
+
+token_base64=$(echo -n $user:$token|base64|perl -n -e 's/\n//; print')
+
+
+if test  "${SPARROW_ADO_DEBUG}" = "1"; then
+
+  cat $cache_dir/request.json
+
+fi
+
+curl -s -L -D - $api/$project/_apis/git/repositories/$repo/refs?api-version=5.0  -o /dev/null \
+-H "Content-Type: application/json" \
+-H "Authorization:  Basic $token_base64" \
+--data-binary  @$cache_dir/request.json | head -n 2
