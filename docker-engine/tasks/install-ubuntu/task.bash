302d2585552589dc68d8a1569924243697856f83
Alexey Melezhik <melezhik@gmail.com>
2020-07-19 16:41:45 -0500

    debian and ubuntu repo fixes

--- docker-engine/tasks/install-ubuntu/task.bash
+++ docker-engine/tasks/install-ubuntu/task.bash
@@ -14,12 +14,12 @@ apt-get -y install \
     software-properties-common
 
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 apt-key fingerprint 0EBFCD88
 
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
