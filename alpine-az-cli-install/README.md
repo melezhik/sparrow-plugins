cb3bf419ce15303301a21579485fe1d2750b9d0f
Alexey Melezhik <melezhik@gmail.com>
2020-10-12 09:04:30 -0500

    new plugin

---
+++ alpine-az-cli-install/README.md
@@ -0,0 +1,15 @@
+# alpine-az-cli-install
+
+Installs az cli on Alpine
+
+# Install
+
+    s6 --install alpine-az-cli-install
+
+# Usage
+
+   s6 --plg-run alpine-az-cli-install
+
+# Author
+
+Alexey Melezhik
---
+++ alpine-az-cli-install/task.bash
@@ -0,0 +1,27 @@
+set -e
+apk add python3
+apk add openssl
+apk add libffi
+apk add libffi
+apk add gcc
+apk add python3-dev
+apk add musl-dev
+apk add libffi-dev
+apk add make
+apk add openssl-dev
+
+curl -sL https://aka.ms/InstallAzureCli > /tmp/az-cli-install.sh
+
+echo "" > /tmp/answers.txt
+echo "" >> /tmp/answers.txt
+echo "" >> /tmp/answers.txt
+echo "" >> /tmp/answers.txt
+echo "" >> /tmp/answers.txt
+echo "" >> /tmp/answers.txt
+echo "" >> /tmp/answers.txt
+
+perl -i -p -e 's{_TTY=/dev/tty}[_TTY=/tmp/answers.txt]' /tmp/az-cli-install.sh
+
+bash /tmp/az-cli-install.sh
+
+
