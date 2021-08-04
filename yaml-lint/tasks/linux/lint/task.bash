18788be96b9b89605a7408c57dc0d40e24ff3d4e
Alexey Melezhik <melezhik@gmail.com>
2021-08-04 16:01:46 +0000

    python3 support

--- yaml-lint/tasks/linux/lint/task.bash
+++ yaml-lint/tasks/linux/lint/task.bash
@@ -6,7 +6,15 @@ echo "check $file ..."
 
 if test $use_python = "True"; then
 
-  if python -c 'import yaml,sys;yaml.safe_load(sys.stdin)' < $file; then
+  if python3 --version 2>&1 1>/dev/null; then
+    py_cmd=python3
+  elif python --version 2>&1 1>/dev/null; then
+    py_cmd=python
+  else
+    echo "neither python nor python3 found"
+    exit
+  fi
+  if $py_cmd -c 'import yaml,sys;yaml.safe_load(sys.stdin)' < $file; then
     echo "$file [OK]" >> $out
   else
     echo "$file [FAIL]" >> $out
