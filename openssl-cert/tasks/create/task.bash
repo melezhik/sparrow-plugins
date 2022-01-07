fdc120d4e94246ed33617c70aa69483dc5026786
Alexey Melezhik <melezhik@gmail.com>
2022-01-07 14:21:04 -0600

    CN issue

--- openssl-cert/tasks/create/task.bash
+++ openssl-cert/tasks/create/task.bash
@@ -1,5 +1,5 @@
-CA=$(config CA)
+CN=$(config CN)
 
 openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
-    -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=$CA" \
+    -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=$CN" \
     -keyout $cache_root_dir/key  -out $cache_root_dir/cert 2>/dev/null
