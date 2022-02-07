58a1b9f56679807d62c06a38cfea806ae8ffadf1
Alexey Melezhik <melezhik@gmail.com>
2022-02-07 10:59:54 -0600

    test group of pods

--- k8s-pod-check/hook.bash
+++ k8s-pod-check/hook.bash
@@ -3,3 +3,7 @@ set -x
 kubectl get pods -n $(config namespace) -o json > $cache_root_dir/state.json
 #set_stdout $cache_root_dir/state.json
 kubectl get pods -n $(config namespace) > $cache_root_dir/data.txt
+
+if test ! $(config die-on-check-fail) = "false"; then
+  ignore_task_check_error
+fi
