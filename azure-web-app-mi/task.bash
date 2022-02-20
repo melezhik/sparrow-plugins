3486023505730d157f923b9097a3d1d36abef378
Alexey Melezhik <melezhik@gmail.com>
2022-02-20 08:32:50 -0600

    new version

--- azure-web-app-mi/task.bash
+++ azure-web-app-mi/task.bash
@@ -5,7 +5,7 @@ app=$(config app)
 
 echo az webapp identity assign -g $rg -n $app
 
-az webapp identity assign -g $rg -n $app > $cache_root_dir/state.json
+az webapp identity assign -g $rg -n $app -o json > $cache_root_dir/state.json
 
 cat $cache_root_dir/state.json
 
