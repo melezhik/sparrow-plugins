e2979d3f1208b9d6964c852465327ab6e687de2a
Alexey Melezhik <melezhik@gmail.com>
2022-03-31 20:11:38 +0000

    redirect stderr to stdout

--- zef/task.bash
+++ zef/task.bash
@@ -10,9 +10,9 @@ for m in "${list[@]}"
 do
   echo zef install $m $options
   if test $m == '(.)'; then
-    zef install . $options
+    zef install . $options 2>&1
   else
-    zef install $m $options
+    zef install $m $options 2>&1
   fi
 done
 
