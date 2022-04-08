72a57665aa6f5cd313415b374c67674b91480dd0
Alexey Melezhik <melezhik@gmail.com>
2022-04-08 15:53:56 +0000

    add sudo

--- sparkyci-service-mysql/README.md
+++ sparkyci-service-mysql/README.md
@@ -4,11 +4,11 @@ Installs mysql service on sparkyci
 
 # Install
 
-    s6 --install sparkyci-service-mysql
+      s6 --install sparkyci-service-mysql
 
 # Usage
 
-   s6 --plg-run sparkyci-service-mysql
+      s6 --plg-run sparkyci-service-mysql
 
 # Author
 
--- sparkyci-service-mysql/task.bash
+++ sparkyci-service-mysql/task.bash
@@ -1,9 +1,9 @@
 set -e
 set -x
 
-apk apk add mysql
-apk add mysql-client
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
-nohup /usr/bin/mysqld_safe &
-echo select 1 | mysql
+sudo apk apk add mysql
+sudo apk add mysql-client
+sudo mysql_install_db --user=mysql --datadir=/var/lib/mysql
+sudo bash -c "nohup /usr/bin/mysqld_safe &"
+sudo bash -c "echo select 1 | mysql"
 
