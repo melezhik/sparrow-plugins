7e54546f7376c77e2def4e1b91c5eb8a099f05f4
Alexey Melezhik <melezhik@gmail.com>
2022-04-12 17:31:00 +0000

    pass parameters

--- sparkyci-service-mysql/README.md
+++ sparkyci-service-mysql/README.md
@@ -20,6 +20,15 @@ After task is complete, mysql service will be started with test database:
 
 * db password - sparky
 
+# Configuration
+
+To changes default values pass parameters:
+
+    task-run "mysql", "sparkyci-service-mysql", %(
+      db_name => "dbdishtest"
+      db_user => "testuser",
+      db_pass => "testpass",
+    );
 
 # Author
 
--- sparkyci-service-mysql/task.bash
+++ sparkyci-service-mysql/task.bash
@@ -1,4 +1,9 @@
 set -e
+
+db_user=$(config db_user)
+db_pass=$(config db_pass)
+db_name=$(config db_name)
+
 set -x
 
 2>&1
@@ -13,14 +18,14 @@ sleep 5
 
 sudo bash -c "echo select 1 | mysql"
 
-sudo mariadb-admin create sparky
+sudo mariadb-admin create $db_name
 
-sudo mysql -e "CREATE USER sparky@'localhost' IDENTIFIED BY 'sparky'"
+sudo mysql -e "CREATE USER $db_user@'localhost' IDENTIFIED BY '$db_pass'"
 
-sudo mysql -e "GRANT ALL ON sparky.* TO 'sparky'@'localhost'"
+sudo mysql -e "GRANT ALL ON $db_name.* TO '$db_user'@'localhost'"
 
-mysql -usparky -psparky -e 'create table foo (a text)' sparky
+mysql -u$db_user -p$db_pass -e 'create table foo (a text)' $db_name
 
-mysql -usparky -psparky -e 'insert into foo (a) values("text")' sparky
+mysql -u$db_user -p$db_pass -e 'insert into foo (a) values("text")' $db_name
 
-mysql -usparky -psparky -e 'select * from foo' sparky
+mysql -u$db_user -p$db_pass -e 'select * from foo' $db_name
