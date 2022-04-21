414c0173bd3eae6b86146a9fbb19f8eacdc9c65a
Alexey Melezhik <melezhik@gmail.com>
2022-04-21 18:04:28 +0000

    python3 dev

--- sparkyci-package-python/task.bash
+++ sparkyci-package-python/task.bash
@@ -6,4 +6,4 @@ set -x
 echo "installing python3/pip packages on sparkyci"
 echo "==========================================="
 
-sudo apk add python3 py3-pip
+sudo apk add python3 py3-pip python3-dev
