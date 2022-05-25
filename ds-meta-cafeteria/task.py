3468f09e3dde0e0b8fcd3c81c26a9814bde37755
Alexey Melezhik <melezhik@gmail.com>
2022-05-25 15:44:57 -0500

    slightly different version

--- ds-meta-cafeteria/task.py
+++ ds-meta-cafeteria/task.py
@@ -16,19 +16,29 @@ def getMaxAdditionalDinersCount(N: int, K: int, M: int, S: List[int]) -> int:
       cnt += calc_cnt(dinner,next_dinner,K)    
   
   print("=============")
-  print(f"LB: 3 | RB: {N-3}")
   if S[0] >= 3:
     print("====================")
     print("handle left boundary")
-    cnt += calc_cnt(-K,S[0],K)
+    cnt += calc_cnt_bnd(1,S[0],K)
 
   if S[M-1] <= (N - 3):
     print("====================")
     print("handle right boundary")
-    cnt += calc_cnt(S[M-1],N+K+1,K)
+    cnt += calc_cnt_bnd(S[M-1],N,K)
 
   print(f"|dinners: {S}, gap(K)={K}, cnt: {cnt}|")
   return cnt
+
+def calc_cnt_bnd (a,b,gap):
+  spots = b - a
+  print(f"[b] a={a} b={b} spots={spots}")
+  if spots > 0:
+    delta = floor((spots /(gap+1)))
+    if delta > 0:
+      print(f"[b] free {delta} spot found between {b} and {a}")
+      return delta
+    else:
+      return 0
 
 def calc_cnt (a,b,gap):
   spots = b - a
