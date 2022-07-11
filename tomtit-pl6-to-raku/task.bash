9772d5497ab48cad80c7601cf1aee074c421c5ec
Alexey Melezhik <melezhik@gmail.com>
2022-07-11 14:30:43 -0500

    bug fix

--- tomtit-pl6-to-raku/task.bash
+++ tomtit-pl6-to-raku/task.bash
@@ -4,7 +4,7 @@ dir=$(config dir)
 
 echo "dir: $dir | renaming *.pl6 to *.raku"
 
-find dir -name "*.pl6" \
+find $dir -name "*.pl6" \
 -exec raku  -e '
   my $a  = "{}"; 
   $a.=subst(/.pl6/,'.raku');
