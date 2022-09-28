f0f8d009768b40ed0148efe6fa0df1199ed5d388
Alexey Melezhik <melezhik@gmail.com>
2022-09-28 17:00:55 -0500

    don't sign

--- sparkyci-tool-apkbuild/tasks/package/task.bash
+++ sparkyci-tool-apkbuild/tasks/package/task.bash
@@ -5,4 +5,3 @@ pkgname=$(config pkgname)
 set -x
 
 su - builder -c "cd /home/builder/ && mkdir -p raku-packages/$pkgname && cd raku-packages/$pkgname && abuild checksum && abuild -r"
-su - builder -c 'abuild-sign /home/builder/packages/raku-packages/x86_64/APKINDEX.tar.gz'
