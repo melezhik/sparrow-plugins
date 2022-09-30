e21486a871337b6f9befc7c5085d825b7425ee19
Alexey Melezhik <melezhik@gmail.com>
2022-09-30 14:19:06 -0500

    proper way to update checksum

--- sparkyci-tool-apkbuild/tasks/package/task.bash
+++ sparkyci-tool-apkbuild/tasks/package/task.bash
@@ -4,4 +4,5 @@ pkgname=$(config pkgname)
 
 set -x
 
+su - builder -c "cd /home/builder/ && mkdir -p raku-packages/$pkgname && cd raku-packages/$pkgname && abuild verify || abuild checksum"
 su - builder -c "cd /home/builder/ && mkdir -p raku-packages/$pkgname && cd raku-packages/$pkgname && abuild -Kcr"
