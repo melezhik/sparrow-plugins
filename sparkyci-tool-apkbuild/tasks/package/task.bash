07ff2465e9aeb7061b9f878f7400d7e7c8cd1bad
Alexey Melezhik <melezhik@gmail.com>
2022-09-30 14:41:41 -0500

    handle new versions

--- sparkyci-tool-apkbuild/tasks/package/task.bash
+++ sparkyci-tool-apkbuild/tasks/package/task.bash
@@ -4,5 +4,4 @@ pkgname=$(config pkgname)
 
 set -x
 
-su - builder -c "cd /home/builder/ && mkdir -p raku-packages/$pkgname && cd raku-packages/$pkgname && abuild verify || abuild checksum"
-su - builder -c "cd /home/builder/ && mkdir -p raku-packages/$pkgname && cd raku-packages/$pkgname && abuild -Kcr"
+su - builder -c "cd /home/builder/ && mkdir -p raku-packages/$pkgname && cd raku-packages/$pkgname; if abuild -c verify; then :; else abuild checksum && abuild -cr; fi;"
