dc7e05acd294c65a7be5bbc78b378ad3567cd479
Alexey Melezhik <melezhik@gmail.com>
2022-09-30 15:02:57 -0500

    we need to use uptodate

--- sparkyci-tool-apkbuild/tasks/package/task.bash
+++ sparkyci-tool-apkbuild/tasks/package/task.bash
@@ -4,4 +4,4 @@ pkgname=$(config pkgname)
 
 set -x
 
-su - builder -c "cd /home/builder/ && mkdir -p raku-packages/$pkgname && cd raku-packages/$pkgname; if abuild -c verify; then :; else abuild checksum && abuild -cr; fi;"
+su - builder -c "cd /home/builder/ && mkdir -p raku-packages/$pkgname && cd raku-packages/$pkgname; abuild -c up2date || abuild -c checksum && abuild -cr"
