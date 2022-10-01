7cfbaeeddfe4fe58f6f016899dc29bbd920a25b6
Alexey Melezhik <melezhik@gmail.com>
2022-10-01 13:34:54 -0500

    use w option

--- sparkyci-tool-apkbuild/tasks/update-apkbuild-file/task.bash
+++ sparkyci-tool-apkbuild/tasks/update-apkbuild-file/task.bash
@@ -2,7 +2,7 @@ set -e
 
 pkgname=$(config pkgname)
 
-if diff -bq "/home/builder/raku-packages/${pkgname}/APKBUILD.new" "/home/builder/raku-packages/${pkgname}/APKBUILD"; then
+if diff -wq "/home/builder/raku-packages/${pkgname}/APKBUILD.new" "/home/builder/raku-packages/${pkgname}/APKBUILD"; then
   echo "keep /home/builder/raku-packages/${pkgname}/APKBUILD ..."
 else
   echo "update /home/builder/raku-packages/${pkgname}/APKBUILD ..."
