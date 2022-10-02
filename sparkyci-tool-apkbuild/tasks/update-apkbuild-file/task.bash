1e1f7324f2ef398e2f89a4a8fb8c2780a3f7b51c
Alexey Melezhik <melezhik@gmail.com>
2022-10-02 13:20:40 -0500

    keep old and new apkbuild files

--- sparkyci-tool-apkbuild/tasks/update-apkbuild-file/task.bash
+++ sparkyci-tool-apkbuild/tasks/update-apkbuild-file/task.bash
@@ -2,10 +2,21 @@ set -e
 
 pkgname=$(config pkgname)
 
-if diff -wt "/home/builder/raku-packages/${pkgname}/APKBUILD.new" "/home/builder/raku-packages/${pkgname}/APKBUILD"; then
-  echo "keep /home/builder/raku-packages/${pkgname}/APKBUILD ..."
+if test -f "/home/builder/raku-packages/${pkgname}/APKBUILD.old"; then
+  if diff -wt "/home/builder/raku-packages/${pkgname}/APKBUILD.new" "/home/builder/raku-packages/${pkgname}/APKBUILD.old"; then
+    echo "keep /home/builder/raku-packages/${pkgname}/APKBUILD ..."
+  else
+    echo "update /home/builder/raku-packages/${pkgname}/APKBUILD ..."
+    cp -v "/home/builder/raku-packages/${pkgname}/APKBUILD.new" "/home/builder/raku-packages/${pkgname}/APKBUILD"
+    cp -v "/home/builder/raku-packages/${pkgname}/APKBUILD.new" "/home/builder/raku-packages/${pkgname}/APKBUILD.old"
+    echo "update checksum for /home/builder/raku-packages/${pkgname}/APKBUILD ..."
+    su - builder -c "cd /home/builder/raku-packages/$pkgname && abuild -c checksum"
+  fi
 else
-  echo "update /home/builder/raku-packages/${pkgname}/APKBUILD ..."
-  cp -v "/home/builder/raku-packages/${pkgname}/APKBUILD.new" "/home/builder/raku-packages/${pkgname}/APKBUILD"
+    echo "update /home/builder/raku-packages/${pkgname}/APKBUILD ..."
+    cp -v "/home/builder/raku-packages/${pkgname}/APKBUILD.new" "/home/builder/raku-packages/${pkgname}/APKBUILD"
+    cp -v "/home/builder/raku-packages/${pkgname}/APKBUILD.new" "/home/builder/raku-packages/${pkgname}/APKBUILD.old"
+    echo "update checksum for /home/builder/raku-packages/${pkgname}/APKBUILD ..."
+    su - builder -c "cd /home/builder/raku-packages/$pkgname && abuild -c checksum"
 fi
 
