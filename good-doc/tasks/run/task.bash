f3c3091712dc9f6ecc0866492923cf133d502fb2
Alexey Melezhik <melezhik@gmail.com>
2022-12-27 14:27:36 -0600

    Update task.bash

--- good-doc/tasks/run/task.bash
+++ good-doc/tasks/run/task.bash
@@ -7,7 +7,7 @@ cat $file
 rakudo_version=$(config rakudo_version)
 
 if ! test -z $rakudo_version; then
- eval "$(rakudo-moar-$version-01-linux-x86_64-gcc/scripts/set-env.sh)"
+ eval "$(rakudo-moar-$rakudo_version-01-linux-x86_64-gcc/scripts/set-env.sh)"
  raku --version 
 fi
 
