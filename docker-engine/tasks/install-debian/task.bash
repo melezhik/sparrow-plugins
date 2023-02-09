1c2a0fa91df0633bb53e27c614fe0b1f41d1efd8
Alexey Melezhik <melezhik@gmail.com>
2023-02-09 13:01:11 -0600

    fix - true -> True

--- docker-engine/tasks/install-debian/task.bash
+++ docker-engine/tasks/install-debian/task.bash
@@ -28,7 +28,7 @@ echo \
 
 sudo apt-get update
 
-if test "$cli_only" = "true"; then
+if test "$cli_only" = "True"; then
   sudo apt-get install -y docker-ce-cli
 else
   sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
