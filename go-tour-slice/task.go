3b4dd75e12001803703c339ad8fa4714bf10d4d0
Alexey Melezhik <melezhik@gmail.com>
2025-01-30 12:28:22 +0300

    new version

--- go-tour-slice/task.go
+++ go-tour-slice/task.go
@@ -14,15 +14,31 @@ func main() {
 
   print_slice("a",s1)
 
+  s2:= make([]int,0,5)
+
+  // alternative:
+  // s2 := s1[:0]
+
+  print_slice("b",s2)
+
+  s3 := make([]int,2,5)
+
+  print_slice("c",s3)
+
+  s4 := s1[2:5]
+
+  print_slice("d",s4)
+
   fmt.Println("<<<")
 }
 
 func print_slice(label string, s []int) {
   fmt.Printf(
-    "<%s len=%d cap=%d %v>\n",
+    "<%s len=%d cap=%d %v> type=%T\n",
     label,
     len(s),
     cap(s),
+    s,
     s,
   )
 }
