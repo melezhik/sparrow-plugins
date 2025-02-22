c0124b44c6798231f13d9f5b70aeaea3b90a8211
Alexey Melezhik <melezhik@gmail.com>
2025-02-22 11:23:48 +0300

    fix yum cache

--- package-generic/tasks/yum/task.bash
+++ package-generic/tasks/yum/task.bash
@@ -2,14 +2,15 @@ package=$(task_var package)
 
 echo trying to install $package ...
 echo os - $os
-echo installer - yum 
+echo installer - yum
 
 if yum -q list installed $package 2>/dev/null ; then
   :
 else
-  yum makecache fast
+  #yum makecache fast
+  yum clean expire-cache
   yum -y install $package || exit 1
-  yum -q list installed $package || exit 1 
+  yum -q list installed $package || exit 1
 fi
 
 
