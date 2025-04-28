410aad432071040630b977e7c8709deaf5b1d6d5
Alexey Melezhik <melezhik@gmail.com>
2025-04-28 16:35:46 +0300

    task.check

--- alpine-mariadb-test/task.bash
+++ alpine-mariadb-test/task.bash
@@ -3,20 +3,24 @@ db_pass=$(config db_pass)
 db_name=$(config db_name)
 
 sudo apk add mariadb-common mariadb-client mariadb
-sudo mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+sudo mariadb-upgrade --user=mysql --datadir=/var/lib/mysql && echo "mariadb initialized"
 sudo bash -c "nohup /usr/bin/mariadbd-safe &"
 sleep 5
 
 sudo bash -c "echo select 1 | mariadb"
 
-sudo mariadb-admin create $db_name 2>&1
+sudo mariadb-admin create $db_name 2>&1 && echo "database created"
 
-sudo mariadb -e "CREATE USER $db_user@'localhost' IDENTIFIED BY '$db_pass'" 2>&1
+sudo mariadb -e "CREATE USER $db_user@'localhost' IDENTIFIED BY '$db_pass'" 2>&1 && echo "user created"
 
 sudo mariadb -e "GRANT ALL ON $db_name.* TO '$db_user'@'localhost'"
 
-mariadb -u$db_user -p$db_pass -e 'create table foo (a text)' $db_name 2>&1
+mariadb -u$db_user -p$db_pass -e 'create table foo (a text)' $db_name 2>&1 && echo "table foo created"
+
+echo "insert_start"
 
 mariadb -u$db_user -p$db_pass -e 'insert into foo (a) values("text")' $db_name
 
 mariadb -u$db_user -p$db_pass -e 'select * from foo' $db_name
+
+echo ""insert_end
