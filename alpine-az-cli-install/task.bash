5ad5313d054df05fba74316c7c23c3ab3d05777f
Alexey Melezhik <melezhik@gmail.com>
2025-07-25 13:34:00 +0300

    remove sudo and replace perl by sed

--- alpine-az-cli-install/task.bash
+++ alpine-az-cli-install/task.bash
@@ -1,14 +1,15 @@
 set -e
-sudo apk add python3
-sudo apk add openssl
-sudo apk add libffi
-sudo apk add libffi
-sudo apk add gcc
-sudo apk add python3-dev
-sudo apk add musl-dev
-sudo apk add libffi-dev
-sudo apk add make
-sudo apk add openssl-dev
+
+apk add python3
+apk add openssl
+apk add libffi
+apk add libffi
+apk add gcc
+apk add python3-dev
+apk add musl-dev
+apk add libffi-dev
+apk add make
+apk add openssl-dev
 
 curl -sL https://aka.ms/InstallAzureCli > /tmp/az-cli-install.sh
 
@@ -20,7 +21,7 @@ echo "" >> /tmp/answers.txt
 echo "" >> /tmp/answers.txt
 echo "" >> /tmp/answers.txt
 
-perl -i -p -e 's{_TTY=/dev/tty}[_TTY=/tmp/answers.txt]' /tmp/az-cli-install.sh
+sed -e 's/_TTY=\/dev\/tty/_TTY=\/tmp\/answers.txt]' /tmp/az-cli-install.sh
 
 bash /tmp/az-cli-install.sh
 
