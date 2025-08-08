859983733b2462b1a2c011ae2d51e780c5c32b14
Alexey Melezhik <melezhik@gmail.com>
2025-08-08 11:28:17 +0300

    debugging

--- group/tasks/create/task.bash
+++ group/tasks/create/task.bash
@@ -5,8 +5,10 @@ if [ $(getent group $name) ]; then
     echo "group $name already exists"
 else
   if [[ $os == alpine ]]; then
+    echo "create group $name"
     addgroup $name
   else
+    echo "create group $name"
     groupadd -f $name
   fi
 fi
