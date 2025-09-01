a3e9cfd2ff79c86f3725328628ce45b4f30589d4
Alexey Melezhik <melezhik@gmail.com>
2025-09-01 10:48:51 +0300

    pass version

--- rakudo-install/install.sh
+++ rakudo-install/install.sh
@@ -1,6 +1,7 @@
-  set -e
-  curl https://rakubrew.org/install-on-perl.sh | sh
-  eval "$(~/.rakubrew/bin/rakubrew init Bash)"
-  rakubrew download $version
-  rakubrew switch $version
-  raku --version
+set -e
+version=$1
+curl https://rakubrew.org/install-on-perl.sh | sh
+eval "$(~/.rakubrew/bin/rakubrew init Bash)"
+rakubrew download $version
+rakubrew switch $version
+raku --version
--- rakudo-install/task.bash
+++ rakudo-install/task.bash
@@ -7,10 +7,10 @@ user=$(config user)
 
 if [[ $user = "" ]]; then
   echo "install rakudo version: $version"
-  bash $task_dir/install.sh
+  bash $task_dir/install.sh $version
 else
   echo "install rakudo version: $version, user: $user"
   cp -v $task_dir/install.sh /tmp/rakudo-install.sh
   chmod a+x /tmp/rakudo-install.sh
-  su - $user -c "bash /tmp/rakudo-install.sh"
+  su - $user -c "bash /tmp/rakudo-install.sh $version"
 fi
