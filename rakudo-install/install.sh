a541103c8e69ad83ee2c07a2b748a421864b91fb
Alexey Melezhik <melezhik@gmail.com>
2025-09-01 10:52:45 +0300

    patching logic

--- rakudo-install/install.sh
+++ rakudo-install/install.sh
@@ -5,3 +5,19 @@ eval "$(~/.rakubrew/bin/rakubrew init Bash)"
 rakubrew download $version
 rakubrew switch $version
 raku --version
+
+if [[ -f ~/.bashrc.patched ]];
+  echo "~/.bashrc already patched"
+else
+  echo "patching ~/.bashrc"
+  echo 'eval "$(~/.rakubrew/bin/rakubrew init Bash)"' >> ~/.bashrc
+  touch ~/.bashrc.patched
+fi
+
+if [[ -f ~/.bash_profile.patched ]];
+  echo "~/.bash_profile already patched"
+else
+  echo "patching ~/.bash_profile"
+  echo 'eval "$(~/.rakubrew/bin/rakubrew init Bash)"' >> ~/.bash_profile
+  touch ~/.bash_profile.patched
+fi
