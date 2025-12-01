483e3a4816870a37367f32d857feb1edcb331979
Alexey Melezhik <melezhik@gmail.com>
2025-12-01 19:13:05 +0300

    bump

--- brownie-logs/task.bash
+++ brownie-logs/task.bash
@@ -4,7 +4,11 @@ path=$(config path)
 
 mkdir -p  ~/.brownie/logs/
 
-for i in $(grep -lRH "add module $m ..." $path); do cp -v $i ~/.brownie/logs/$i.log; done
+rm -rf /tmp/log
+
+for i in $(grep -lRH "add module $m ..." $path); do cat $i >> /tmp/log; done
+
+cp /tmp/log ~/.brownie/logs/$m.log
 
 echo
 
