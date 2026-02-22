9f5d493e5afb1ee8a00a7430ff2cba64b4a5da1b
Alexey Melezhik <melezhik@gmail.com>
2026-02-22 21:19:25 +0300

    Fix me

--- .dsci/task.bash
+++ .dsci/task.bash
@@ -1,5 +1,7 @@
 set -e
 
+export SP6_REPO=file://$HOME/repo
+
 new_commit=$(config DSCI_COMMIT)
 
 old_commit=$(config DSCI_COMMIT)
@@ -8,11 +10,14 @@ if test -f ~/sparrow-plugins.commit; then
   old_commit=$(cat ~/sparrow-plugins.commit)
 fi
 
-chdir ../
+chdir ../git-updated-dirs
+
+s6 --upload
+s6 --index-update
+
+cd ../
 
 eval $(s6 --plg-run --inline git-updated-dirs@dir=.,commit_new=$new_commit,commit_old=$old_commit,level=0)
-
-export SP6_REPO=file://$HOME/repo
 
 for d in "${dirs[@]}"; do
   echo "Processing: $d"
